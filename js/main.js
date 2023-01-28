f1c7d8630227ba79a11babdeca14a2fca5534a52
MyLadySnezhka <mylady.snezhka@gmail.com>
2023-01-29 01:49:00 +0200

    add Moment library

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 const elSelectSex = document.querySelector('select[name=sex_select]');
 const elAge = document.querySelector('.age_block');
 const elAnswer = document.querySelector('.answer');
+const elInputYear = document.querySelector('input[name=year]');
+const elComment = document.querySelector('.comment');
+const elBtnClose = document.querySelector('button[name=btn_close');
 
 elSelectSex.addEventListener('change', (ev) => {
     let sex = ev.target.value;
     if (sex === 'female') {
-        elAnswer.innerHTML = 'Яка ж гарненька киця!';
+        elAnswer.innerHTML = `<p>Яка ж ти гарненька киця! <br> У жінок про вік не питають!</p>`;
         elAge.classList.remove('show');
     } else if (sex === 'male') { 
         elAnswer.innerHTML = 'Якого ти року народження?';
@@ -14,8 +17,32 @@ elSelectSex.addEventListener('change', (ev) => {
         elAnswer.innerHTML = 'Здається, нічого не обрано...'
         elAge.classList.remove('show');
     };
-})
+});
 
-elAge.addEventListener('change', (ev) => {
-    const year = ev.target.value;
-})
+elInputYear.addEventListener('keyup', (ev) => {
+    elInputYear.value = elInputYear.value.replace(/[^0-9.]/g, '');
+    const year = Number(ev.target.value);
+    if (year < '1900') {
+        elComment.innerHTML = 'Твій вік занадто стародавній. Ти часом не динозавр? Вкажи реальний рік!';
+        return;
+    }
+    if (year > '2023') {
+        elComment.innerHTML = 'Агов! Ти ще не народився?? Вкажи правильний рік!';
+        return;
+    }
+    //console.log(year, typeof year);
+    const time = moment().format('YYYY'); 
+    let age = time - year;
+    elComment.innerHTML = `Тобі ${age} років.`;
+    if (age < 18) {elComment.insertAdjacentHTML("beforeend", `<p>Тому ще зарано таке дивитися!</p>`)}
+    else {elComment.insertAdjacentHTML("beforeend", `<p>Сідай зручніше, обирай цікавіше!</p>`)};
+});
+
+elBtnClose.addEventListener('click', () => {
+    elInputYear.value = '';
+    elComment.innerHTML = '';
+});
+
+
+
+

e815c33a41229fae9d52efc01d020538dae7c7f4
MyLadySnezhka <mylady.snezhka@gmail.com>
2023-01-28 16:52:09 +0200

    add remove show

--- js/main.js
+++ js/main.js
@@ -6,10 +6,14 @@ elSelectSex.addEventListener('change', (ev) => {
     let sex = ev.target.value;
     if (sex === 'female') {
         elAnswer.innerHTML = 'Яка ж гарненька киця!';
+        elAge.classList.remove('show');
     } else if (sex === 'male') { 
         elAnswer.innerHTML = 'Якого ти року народження?';
         elAge.classList.add('show');
-    } else {elAnswer.innerHTML = 'Здається, нічого не обрано...'};
+    } else {
+        elAnswer.innerHTML = 'Здається, нічого не обрано...'
+        elAge.classList.remove('show');
+    };
 })
 
 elAge.addEventListener('change', (ev) => {
